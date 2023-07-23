d9394b1cd42c937810de2f70136d3abb0e28314c
Michael Hansen <mihansen@microsoft.com>
2023-07-23 15:23:17 +0000

    Bootstrap for python codegen

---
+++ python/mrd_stream_recon.py
@@ -0,0 +1,24 @@
+import argparse
+from typing import BinaryIO
+import sys
+import mrd
+
+def reconstruct_mrd_stream(input: BinaryIO, output: BinaryIO):
+    with mrd.BinaryMrdReader(input) as reader:
+        with mrd.BinaryMrdWriter(output) as writer:
+            head = reader.read_header()
+            writer.write_header(head)
+            writer.write_data(reader.read_data())
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Reconstructs an MRD stream")
+    parser.add_argument('-i', '--input', type=str, required=False, help="Input file, defaults to stdin")
+    parser.add_argument('-o', '--output', type=str, required=False, help="Output file, defaults to stdout")
+    args = parser.parse_args()
+
+    input = open(args.input, "rb") if args.input is not None else sys.stdin.buffer
+    output = open(args.output, "wb") if args.output is not None else sys.stdout.buffer
+
+    reconstruct_mrd_stream(input, output)
