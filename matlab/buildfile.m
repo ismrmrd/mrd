4648282c241427e3cd6ddbd80b7c36f57000adee
Joe Naegele <naegelejd@users.noreply.github.com>
2024-04-18 16:50:14 -0400

    Fix MATLAB toolbox build task (#2)

--- matlab/buildfile.m
+++ matlab/buildfile.m
@@ -2,20 +2,14 @@ function plan = buildfile
     import matlab.buildtool.tasks.CodeIssuesTask
     import matlab.buildtool.tasks.TestTask
 
-    % Create a plan from task functions
     plan = buildplan(localfunctions);
-
-    % Add a task to identify code issues
     plan("check") = CodeIssuesTask(Results="tests/results/static-analysis-results.sarif");
-
-    % Add a task to run tests
     plan("test") = TestTask("tests", SourceFiles="toolbox", TestResults="tests/results/test-results.xml");
 
-    % Make the "packageToolbox" task the default task in the plan
-    plan.DefaultTasks = "packageToolbox";
+    plan.DefaultTasks = ["check", "test"];
 
     % Make the "packageToolbox" task dependent on the "check" and "test" tasks
-    plan("packageToolbox").Dependencies = ["check" "test"];
+    % plan("packageToolbox").Dependencies = ["check" "test"];
 end
 
 function packageToolboxTask(~)
